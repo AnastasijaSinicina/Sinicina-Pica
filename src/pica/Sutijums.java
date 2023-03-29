3c422ca0d68b12a13be0c45636d7014abe5527f0
Anastasija <sinica.nastja@gmail.com>
2023-03-29 14:12:11 +0300

    Ievietojam visas pārbaudes, piem(tel. numura jābūt 8 cipari)

--- src/pica/Sutijums.java
+++ src/pica/Sutijums.java
@@ -1,6 +1,26 @@
 package pica;
 
 public class Sutijums extends pica{
-	int izveletaisIndekss;
+	String vards, adrese, num, gNum;
+	String veids="", izm="", dzeriens="", piedeva="", merce="";
+	double cena=0;
+	int piegade;
+	 
+	public Sutijums(String vards, String adrese,String num, 
+			String gNum, String veids, String izm,String dzeriens, 
+			String piedeva,String merce, double cena, int piegade) {
+		
+		this.vards = vards;
+		this.adrese = adrese;
+		this.num = num;
+		this.gNum = gNum;
+		this.izm = izm;
+		this.dzeriens = dzeriens;
+		this.piedeva = piedeva;
+		this.merce = merce;
+		this.cena = cena;
+		this.piegade = piegade;
+		
+	}
 
 }
--- src/pica/pica.java
+++ src/pica/pica.java
@@ -8,6 +8,15 @@ import javax.swing.JOptionPane;
 public class pica {
 	static int izveletaisIndekss;
 	static ArrayList<Sutijums> klienti = new ArrayList<Sutijums>();
+	static String vards, adrese, num, gNum;
+	static String veids="";
+	static String izm="";
+	static String dzeriens="";
+	static String piedeva="";
+	static String merce="";
+	double cena=0;
+	static int piegade;
+	 
 	
 	public static void main(String[] args) {
 		metodes();
@@ -35,6 +44,72 @@ public class pica {
 		}
 	}
 	private static void saglabat() {
+		piegade = JOptionPane.showConfirmDialog(null, "Būs piegāde?", "Pizza_YUMMY", JOptionPane.YES_NO_OPTION);
+		String[] veidi= {"Studentu", "Margarita","Havaju", "Kalifornijas"};
+		String[] izmers = {"25cm", "30cm", "50cm"};
+		String[] dzerieni = {"Coca-Cola", "Fanta", "Sprite",
+				"Ābolu sula", "Tomātu sula", "Vīnogu sula"};
+		String[] piedevas = {"Frī kartupeļi", "Sīpolu gredzeni", "Ķiploku maizītes"};
+		String[] merces = {"Ketčups", "Majonēze", "BBQ", "Gurķu"};
+		
+		String[] pasut = new String[20];
+		int sutSk = 0;
+		
+		
+		
+		int picSk = Integer.parseInt(JOptionPane.showInputDialog
+				(null,"Cik picas?", "Pizza_YUMMY", JOptionPane.QUESTION_MESSAGE));
+		while(picSk!=0) {
+			if(picSk>0) {
+				veids = (String)JOptionPane.showInputDialog(null, "Picca:", "Pizza_YUMMY", JOptionPane.PLAIN_MESSAGE, null, veidi, veidi[0]);
+				izm = (String)JOptionPane.showInputDialog(null, "Izmers:", "Pizza_YUMMY", JOptionPane.PLAIN_MESSAGE, null, izmers, izmers[0]);
+               
+				pasut[sutSk] = veids +" "+izm;
+				sutSk++;
+				picSk--;
+			}
+		}
+		
+		int dzSk = Integer.parseInt(JOptionPane.showInputDialog
+				(null,"Cik dzērienu?", "Pizza_YUMMY", JOptionPane.QUESTION_MESSAGE));
+		while(dzSk!=0) {
+			if(dzSk>0) {
+				dzeriens = (String)JOptionPane.showInputDialog(null, "Dzeriens:", "Pizza_YUMMY", JOptionPane.PLAIN_MESSAGE, null, dzerieni, dzerieni[0]);              
+				pasut[sutSk] = dzeriens;
+				sutSk++;
+				dzSk--;
+			}
+		}
+		int mercSk = Integer.parseInt(JOptionPane.showInputDialog
+				(null,"Cik dzērienu?", "Pizza_YUMMY", JOptionPane.QUESTION_MESSAGE));
+		while(mercSk!=0) {
+			if(mercSk>0) {
+				merce = (String)JOptionPane.showInputDialog(null, "Dzeriens:", "Pizza_YUMMY", JOptionPane.PLAIN_MESSAGE, null, merces, merces[0]);              
+				pasut[sutSk] = dzeriens;
+				sutSk++;
+				mercSk--;
+			}
+		}
+		int piedSk = Integer.parseInt(JOptionPane.showInputDialog
+				(null,"Cik dzērienu?", "Pizza_YUMMY", JOptionPane.QUESTION_MESSAGE));
+		while(piedSk!=0) {
+			if(piedSk>0) {
+				piedeva = (String)JOptionPane.showInputDialog(null, "Dzeriens:", "Pizza_YUMMY", JOptionPane.PLAIN_MESSAGE, null, piedevas, piedevas[0]);              
+				pasut[sutSk] = dzeriens;
+				sutSk++;
+				piedSk--;
+			}
+		}
+		if(piegade == JOptionPane.YES_OPTION) {
+			adrese = JOptionPane.showInputDialog(null, "Adrese:", "Pizza_YUMMY");
+			vards = JOptionPane.showInputDialog(null, "Vards:", "Pizza_YUMMY");
+			num = JOptionPane.showInputDialog(null, "Tālr.:", "Pizza_YUMMY");
+
+
+		}
+		
+		
+		
 		
 	}
    private static void izvadit() {
